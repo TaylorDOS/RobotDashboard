4db6f46db2c079332a017161ec47e801cf6f1e09
TaylorTan1998 <haoyi2@hotmail.com>
2024-11-15 11:11:14 +0800

    ui design

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -6,17 +6,18 @@ interface DominoTrackerProps {
   
   const Tracker: React.FC<DominoTrackerProps> = ({ steps, completedSteps }) => {
     return (
-      <div className="flex justify-between w-full items-center gap-x-4">
+        <div className="flex flex-col lg:flex-row justify-between w-full items-center gap-x-4 gap-y-4 lg:gap-y-0">
         {steps.map((step, index) => (
           <div
             key={index}
-            className={`flex-grow h-12 rounded-full flex items-center justify-center
+            className={`w-full flex-grow h-12 rounded-full flex items-center justify-center
               ${index < completedSteps ? 'bg-green-500 text-white' : 'bg-gray-300 text-gray-600'}`}
           >
             {step}
           </div>
         ))}
       </div>
+      
     );
   };
   
