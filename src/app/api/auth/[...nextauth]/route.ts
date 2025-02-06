fe32d1b45f470cd1ccb8190e70b815ff172409c4
TaylorTan1998 <haoyi2@hotmail.com>
2025-02-06 23:30:13 +0800

    test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,22 +9,6 @@ const handler = NextAuth({
       issuer: process.env.COGNITO_ISSUER!,
     }),
   ],
-  callbacks: {
-		async jwt({ token, user, account, profile, isNewUser }) {
-			if (user) {
-				token.id = user.id
-			}
-			if (account) {
-				token.accessToken = account.access_token
-			}
-			return token
-		},
-		async session({ session, token, user }) {
-			// @ts-expect-error
-			session.token = token.accessToken
-			return session
-		},
-	},
 	secret: process.env.NEXTAUTH_SECRET,
 });
 
